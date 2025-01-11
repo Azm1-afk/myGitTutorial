6ccea37a6148ab8b8d023819bc373ebf9566821b
Azm1-afk <abdullahilazmi@gmail.com>
2025-01-11 23:04:33 +0600

    added a third function

--- demo.py
+++ demo.py
@@ -14,3 +14,18 @@ else:
     print('This is an odd number')
     
 #added a function just to test git
+
+# Python program to square and cube every number in a given list of integers using Lambda.
+# Original list of integers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+given_List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+squareFunc = lambda a: a*a
+cubeFunc = lambda a: a*a*a
+
+mapFunc1 = lambda jinish: list(map(squareFunc, jinish))
+mapFunc2 = lambda jinish: list(map(cubeFunc, jinish))
+
+
+print(mapFunc1(given_List))
+print(mapFunc2(given_List))
