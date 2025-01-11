7043598b926a639d3455a7767a109ab6f5e1b539
Azm1-afk <abdullahilazmi@gmail.com>
2025-01-11 22:38:49 +0600

    added some new code to file

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
 #this is just a git tutorial for myself
 
 print('helloWorld')
+
+#Exercise 1: simple function in Python
+print('Exercise 1:')
+def checkOdd(num1):
+    if(num1 % 2 == 0):
+        return True
+    
+if(checkOdd(3)):
+    print('This is not an odd number.')
+else:
+    print('This is an odd number')
